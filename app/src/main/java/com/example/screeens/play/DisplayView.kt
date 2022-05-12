970df73e2b2b2613017b1484a3b86fa2d713d420
likus <zm0963@gmail.com>
2022-05-12 12:19:15 +0300

    Added ugly checkbox

--- app/src/main/java/com/example/screeens/play/DisplayView.kt
+++ app/src/main/java/com/example/screeens/play/DisplayView.kt
@@ -3,6 +3,7 @@ package com.example.screeens.play
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material.Button
+import androidx.compose.material.Checkbox
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
@@ -18,12 +19,21 @@ fun DisplayView(
             item {
                 Text(
                     text = "${viewState.question}",
-                    modifier = Modifier.padding(bottom = 10.dp, top = 10.dp)
+                    modifier = Modifier.padding(10.dp)
                 )
                 viewState.answerList.forEach {
-                    Text(text = viewState.answerList.component1().answer1)
-                    Text(text = viewState.answerList.component1().answer2)
-                    Text(text = viewState.answerList.component1().answer3)
+                    Row() {
+                        Checkbox(checked = false, onCheckedChange = {})
+                        Text(text = viewState.answerList.component1().answer1)
+                    }
+                    Row() {
+                        Checkbox(checked = false, onCheckedChange = {})
+                        Text(text = viewState.answerList.component1().answer2)
+                    }
+                    Row() {
+                        Checkbox(checked = false, onCheckedChange = {})
+                        Text(text = viewState.answerList.component1().answer3)
+                    }
                 }
             }
         }
